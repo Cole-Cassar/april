44a28a1c6358433199809cad943d976e3690c568
Cole <Cole@example.com>
2023-10-19 16:04:27 -0600

    Step 3 - WIP

--- temp.py
+++ temp.py
@@ -1,8 +1,18 @@
-def celsius_to_fahrenheit():
+"""
+    Program to convert Celsius to Fahrenheit
+"""
+def celsius_to_fahrenheit(celsius):
     '''Calculates Celsius to Fahrenheit'''
-    fah = 25 * 9/5 + 32
+    fah = celsius * 9/5 + 32
     return fah
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = fahrenheit - 32
+    return celsius
 # Program to convert Celcius to Fahrenheit
 celsius = 25
 fah = celsius_to_fahrenheit(celsius)
 print(f"{celsius} degrees is {fah} Fahrenheit")
+
+celsius = fahrenheit_to_celsius(fah)
+print(f"{fah} degrees Fahrenheit is {celsius} Celsius")
